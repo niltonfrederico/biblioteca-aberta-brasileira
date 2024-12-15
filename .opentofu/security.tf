3c81749c9399b19b952e8d9ad7cb7a77da22b7b4
Nilton Frederico Teixeira <9078708+niltonfrederico@users.noreply.github.com>
2024-12-15 17:30:52 -0300

    feat: added security and routing

---
+++ .opentofu/security.tf
@@ -0,0 +1,26 @@
+# Security Groups
+resource "aws_security_group" "lambda" {
+  name        = "lambda"
+  description = "Security group for Lambda functions"
+  vpc_id      = aws_vpc.main.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "aurora" {
+  name        = "aurora"
+  description = "Security group for Aurora cluster"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.lambda.id]
+  }
+}
